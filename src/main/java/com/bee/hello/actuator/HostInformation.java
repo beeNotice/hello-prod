0b00cc78e5ba94d5c69c4e8f92779eb8aeb921ba
Fabien MARTIN <fabien.martin@beenotice.com>
2021-04-28 13:36:52 +0200

    Working AKS version

---
+++ src/main/java/com/bee/hello/actuator/HostInformation.java
@@ -0,0 +1,29 @@
+package com.bee.hello.actuator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.stereotype.Component;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+// https://docs.spring.io/spring-boot/docs/current/reference/html/production-ready-features.html#production-ready-application-info-custom
+@Component
+public class HostInformation implements InfoContributor {
+
+    private static final Logger LOG = LoggerFactory.getLogger(HostInformation.class);
+
+    @Override
+    public void contribute(Info.Builder builder) {
+
+        String host = "Unknown";
+        try {
+            host = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            LOG.warn("Cannot access to the host address", e);
+        }
+        builder.withDetail("host", host);
+    }
+}
